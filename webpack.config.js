3e179319310fedaf9b8d809770b2148a2c602f04
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2018-04-08 19:29:05 -0600

    indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-      contentBase: './dist'
-  },
-  plugins: [
-      new HtmlWebpackPlugin({
-          filename: 'index.html',
-          template: './index.html'
-      })
-  ],
-  module: {
-      rules: [
-          {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: {
-                  loader: 'babel-loader'
-              }
-          }
-      ]
-  }
+    entry: [
+        'babel-polyfill', 
+        './src/index.js'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './index.html'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    }
 };
