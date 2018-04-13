094debcab48f19ceb564e47c62c4b871c6fb7570
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2018-04-12 22:52:27 -0600

    js base filas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 import _ from 'lodash';
+import './styles/style.css';
 
-function component() {
-    var element = document.createElement('div');
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'World'], ' ');
+var indexCtl = (function() {
 
-    return element;
-}
+})();
 
-document.body.appendChild(component());
+var uiCtl = (function() {
+
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container',
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
+    };
+
+    function getDOMStrings() {
+        return DOMStrings;
+    }
+
+    return {
+        getDOMStrings: getDOMStrings
+    };
+
+})();
+
+var appCtl = (function(_indexCtl, _uiCtl) {
+
+    var DOM = _uiCtl.getDOMStrings();
+
+    document.querySelector(DOM.inputBtn).onclick = ctlAddItem;
+    document.onkeypress = function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctlAddItem();
+        }
+    };
+
+    function ctlAddItem() {
+        console.log('hola');
+    }
+    function init() {
+    }
+
+    return {
+        init: init
+    };
+
+})(indexCtl, uiCtl);
